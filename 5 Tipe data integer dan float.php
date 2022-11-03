58c3b35f90c6837b6cc919db1918f87ccd899a71
Dani-Mulyo-Febrianto <94659142+Dani-Mulyo-Febrianto@users.noreply.github.com>
2022-11-03 11:19:07 +0700

    Add files via upload

---
+++ 5 Tipe data integer dan float.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+
+      //tipe data angka/number
+      //1. integer 2. float
+
+      $angka = 1000;
+      $angka2 = 100.123;
+
+      echo $angka + $angka2;
+    ?> 
+
+   
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
