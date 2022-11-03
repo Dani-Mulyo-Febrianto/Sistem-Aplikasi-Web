954f382449b112a48a78e2c0fcf04981725d25ea
DANI M. FEBRIANTO <94659142+Dani-Mulyo-Febrianto@users.noreply.github.com>
2022-11-03 02:25:56 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>Belajar PHP</title>
+  </head>
+  <body>
+    <?php
+
+      echo "<h1>Sekolah Koding</h1>";
+      echo "Halo <br>";
+      echo "Member";
+     
+    ?> 
+    Halo Semuanya !
+    ini Menggunakan Komentar
+    
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
